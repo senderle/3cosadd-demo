28c04b1b2a983fd2d514bb1154c822a31b2f4529
Scott Enderle <scott.enderle@gmail.com>
2020-01-16 15:20:34 -0500

    handle multiple correct answers with an any/any strategy

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy
 import sys
+import os
 
 def load_vecs(path):
     with open(path) as ip:
@@ -11,13 +12,18 @@ def load_vecs(path):
 
 def load_test(path):
     with open(path) as ip:
-        pairs = [line.split() for line in ip]
-    words = [w for p in pairs for w in p]
+        pairs = [l.split() for l in ip]
+        pairs = [[a, a_.split('/')] for a, a_ in pairs]
+    words = [[a] + a_ for a, a_ in pairs]
+    words = [w for aa_ in words for w in aa_]
     words_ix = {w: i for i, w in enumerate(words)}
+
     return words, words_ix, pairs
 
 def precompute_sims(vecs, vec_words_ix, test_words):
-    test_vecs = numpy.array([vecs[vec_words_ix[t]] for t in test_words])
+    test_vecs = numpy.array([
+        vecs[vec_words_ix[t]] if t in vec_words_ix else [0] * len(vecs[0]) 
+             for t in test_words])
     return vecs @ test_vecs.T
 
 def perform_test(sims, vec_words, vec_words_ix, test_words_ix, a, a_, b):
@@ -26,22 +32,47 @@ def perform_test(sims, vec_words, vec_words_ix, test_words_ix, a, a_, b):
     ix_b = test_words_ix[b]
 
     scores = sims[:, ix_a_] - sims[:, ix_a] + sims[:, ix_b]
-    scores[vec_words_ix[a]] = 0
-    scores[vec_words_ix[a_]] = 0
-    scores[vec_words_ix[b]] = 0
+
+    if a in vec_words_ix:
+        scores[vec_words_ix[a]] = 0
+    if a_ in vec_words_ix:
+        scores[vec_words_ix[a_]] = 0
+    if b in vec_words_ix:
+        scores[vec_words_ix[b]] = 0
 
     return vec_words[scores.argmax()]
 
 if __name__ == '__main__':
     vec_path = sys.argv[1] if len(sys.argv) > 1 else 'sample-vectors.txt'
-    test_path = sys.argv[2] if len(sys.argv) > 2 else 'sample-test.txt'
+    test_paths = sys.argv[2:] if len(sys.argv) > 2 else ['sample-test.txt']
+
     vec_words, vec_words_ix, vecs = load_vecs(vec_path)
-    test_words, test_words_ix, test_pairs = load_test(test_path)
 
-    sims = precompute_sims(vecs, vec_words_ix, test_words)
+    all_results = []
+    for path in test_paths:
+        test_words, test_words_ix, test_pairs = load_test(path)
 
-    results = []
-    for a, a_ in test_pairs:
-        for b, b_ in test_pairs:
-            results.append(perform_test(sims, vec_words, vec_words_ix, test_words_ix, a, a_, b) == b_)
-    print('{} out of {} correct'.format(sum(results), len(results)))
+        if any('/' in w for w in test_words):
+            print('Tests with multiple answers are not supported yet. Skipping {}'.format(path))
+            continue
+
+        sims = precompute_sims(vecs, vec_words_ix, test_words)
+        results = []
+        for a, a_ in test_pairs:
+            for b, b_ in test_pairs:
+                b_ = set(b_)
+                pair_results = []
+                for w_a_ in a_:
+                    guess = perform_test(sims, vec_words, vec_words_ix, test_words_ix, a, w_a_, b)
+                    pair_results.append(guess in b_)
+                results.append(any(pair_results))
+
+        all_results.extend(results)
+        filename = os.path.split(path)[-1]
+        print('{}: {:.4g} out of {} correct ({:.4g})'.format(
+            filename, sum(results), len(results), sum(results) / len(results)
+        ))
+
+    print('Collected results: {:.4g} out of {} correct ({:.4g})'.format(
+        sum(all_results), len(all_results), sum(all_results) / len(all_results)
+    ))
